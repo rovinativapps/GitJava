94d514afd1c9cd6a346ae6dbee46e4f9de5ac3bd
Rovi Acuña <rovi@nativapps.com>
2016-07-18 20:49:45 -0500

    Bienvenida corregida de forma adecuada

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -15,7 +15,7 @@ public class JavaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.err.println("Bienvenido a mi aplicaicón");
+        System.err.println("Bienvenido a mi aplicación");
     }
     
 }
