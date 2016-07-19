cd03c1cc950f2d8cda7aee00dcbc11a79b9370d2
Rovi Acuña <rovi@nativapps.com>
2016-07-18 20:48:30 -0500

    Se ha agregado el mensaje de bienvenida

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -15,7 +15,7 @@ public class JavaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("Bienvenido a mi aplicaicón");
     }
     
 }
